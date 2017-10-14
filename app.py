52ec29a4aa813e1e52bcc8139328211070fcc20e
Volney Casas <volneyrock@gmail.com>
2017-10-14 17:14:13 -0300

    exibe: "Frase: >>> classificação"

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ dataset = pd.read_csv('tweets.csv')
 
 # Separa tweets das classificações
 tweets = dataset['Text'].values
-classes = dataset['Classificacao'].values
+classi = dataset['Classificacao'].values
 
 
 # Treina o modelo usando algorítimo Naive Bayes Multinomial #
@@ -19,9 +19,9 @@ classes = dataset['Classificacao'].values
 # vectorizer = CountVectorizer(analyzer='word')#Analiza palavra por palavra
 vectorizer = CountVectorizer(ngram_range=(1,2))#Analiza pares de palavras
 freq_tweets = vectorizer.fit_transform(tweets)
-# Treina o modelo usando a frequencia de palavras e as classes de cada instância
+# Treina o modelo usando a frequencia de palavras e as classi de cada instância
 modelo = MultinomialNB()
-modelo.fit(freq_tweets, classes)
+modelo.fit(freq_tweets, classi)
 
 
 # Testes #
@@ -32,11 +32,14 @@ testes = ['Adorei o novo Hamburguer do Tucano',
          ]
 
 freq_testes = vectorizer.transform(testes)
-print(modelo.predict(freq_testes))
+# print(modelo.predict(freq_testes))
+for i in testes:
+    index = testes.index(i)
+    print(i + ': >>>' + modelo.predict(freq_testes)[index])
 
 # Validação do modelo
-resultados = cross_val_predict(modelo, freq_tweets, classes, cv=10)
-print("Acurácia: {0}%".format(str(metrics.accuracy_score(classes, resultados))[2:4]))
+resultados = cross_val_predict(modelo, freq_tweets, classi, cv=10)
+print("Acurácia: {0}%".format(str(metrics.accuracy_score(classi, resultados))[2:4]))
 # Matriz de confusão
 print('Matriz de confusão:')
-print (pd.crosstab(classes, resultados, rownames=['Real'], colnames=['Predito'], margins=True), '')
+print (pd.crosstab(classi, resultados, rownames=['Real'], colnames=['Predito'], margins=True), '')
